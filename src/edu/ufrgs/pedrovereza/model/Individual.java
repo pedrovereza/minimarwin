baa9788c2dbfc47ddde3de81c016fbe8d5af8239
Pedro Vereza <pdrvereza@gmail.com>
2015-04-23 23:21:51 -0300

    Cross over function and refactoring
    Adds the function to process crossover of two Individuals. Also
    extracts a bunch of magic numbers into constants.

--- src/edu/ufrgs/pedrovereza/model/Individual.java
+++ src/edu/ufrgs/pedrovereza/model/Individual.java
@@ -1,8 +1,10 @@
 package edu.ufrgs.pedrovereza.model;
 
+import static java.lang.System.arraycopy;
+import static java.util.Collections.emptyList;
 import com.lagodiuk.ga.Chromosome;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -15,8 +17,11 @@ public class Individual implements Chromosome<Individual> {
 
     private static final int Y_PHENOTYPE_START = 12;
     private static final int Y_PHENOTYPE_END = 23;
-    public static final double MINIMUM_VALUE = -2.0;
-    public static final double MAXIMUM_VALUE = 4.0;
+    private static final double MINIMUM_VALUE = -2.0;
+    private static final double MAXIMUM_VALUE = 4.0;
+    private static final int CHROMOSOME_SIZE = 24;
+    public static final double TWENTY_FIVE_PERCENT = 0.25;
+    public static final double TEN_PERCENT = 0.1;
 
     private byte[] chromosome;
     private final Random random;
@@ -26,20 +31,71 @@ public class Individual implements Chromosome<Individual> {
     }
 
     public Individual(byte[] chromosome, Random random) {
-        this.chromosome = chromosome;
+        this.chromosome = chromosome.clone();
         this.random = random;
     }
 
     public double phenotypeForX() {
         double xValue = binaryToDecimal(X_PHENOTYPE_START, X_PHENOTYPE_END);
-
         return decimalToDomainRange(xValue);
     }
 
     public double phenotypeForY() {
         double yValue = binaryToDecimal(Y_PHENOTYPE_START, Y_PHENOTYPE_END);
-
         return decimalToDomainRange(yValue);
+    }
+
+    @Override
+    public List<Individual> crossover(Individual individual) {
+        if (random.nextDouble() < TWENTY_FIVE_PERCENT) {
+            return emptyList();
+        }
+
+        int positionToCrossover = (int) (random.nextDouble() * (CHROMOSOME_SIZE - 1));
+
+        return crossoverAtPosition(individual, positionToCrossover);
+    }
+
+    @Override
+    public Individual mutate() {
+        byte[] newChromosome = chromosome.clone();
+
+        if (random.nextDouble() > TEN_PERCENT) {
+            mutateAtRandomPosition(newChromosome);
+        }
+
+        return new Individual(newChromosome, random);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o.getClass() != Individual.class) {
+            return false;
+        }
+
+        Individual other = (Individual)o;
+
+        return  phenotypeForX() == other.phenotypeForX() &&
+                phenotypeForY() == other.phenotypeForY();
+    }
+
+    private List<Individual> crossoverAtPosition(Individual individual, int positionToCrossover) {
+        List<Individual> offspring = new ArrayList<Individual>(2);
+        byte[] firstChromosome = chromosome.clone();
+        byte[] secondChromosome = chromosome.clone();
+
+        arraycopy(individual.chromosome, 0, firstChromosome, 0, positionToCrossover);
+
+        arraycopy(individual.chromosome, positionToCrossover, secondChromosome, positionToCrossover,
+                CHROMOSOME_SIZE - positionToCrossover);
+
+        Individual firstIndividual = new Individual(firstChromosome, random);
+        Individual secondIndividual = new Individual(secondChromosome, random);
+
+        offspring.add(firstIndividual);
+        offspring.add(secondIndividual);
+
+        return offspring;
     }
 
     private double binaryToDecimal(int start, int end) {
@@ -56,24 +112,8 @@ public class Individual implements Chromosome<Individual> {
         return MINIMUM_VALUE + (decimal * (MAXIMUM_VALUE /(4096 -1)));
     }
 
-    @Override
-    public List<Individual> crossover(Individual individual) {
-       return null;
-    }
-
-    @Override
-    public Individual mutate() {
-        byte[] newChromosome = Arrays.copyOf(chromosome, 24);
-
-        if (random.nextDouble() > 0.1) {
-            mutateAtRandomPosition(newChromosome);
-        }
-
-        return new Individual(newChromosome, random);
-    }
-
     private void mutateAtRandomPosition(byte[] newChromosome) {
-        int positionToMutate = (int) (random.nextDouble() * 23);
+        int positionToMutate = (int) (random.nextDouble() * (CHROMOSOME_SIZE - 1));
 
         newChromosome[positionToMutate] = (byte) ((newChromosome[positionToMutate] + 1) % 2);
     }
--- test/edu/ufrgs/pedrovereza/model/IndividualTest.java
+++ test/edu/ufrgs/pedrovereza/model/IndividualTest.java
@@ -1,13 +1,14 @@
 package edu.ufrgs.pedrovereza.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-import edu.ufrgs.pedrovereza.model.FakeRandom;
-import edu.ufrgs.pedrovereza.model.Individual;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class IndividualTest {
 
@@ -56,7 +57,7 @@ public class IndividualTest {
 
         Individual newIndividual = individual.mutate();
 
-        assertEquals(newIndividual.phenotypeForX(), individual.phenotypeForX(), 0.0);
+        assertEquals(newIndividual, individual);
     }
 
     @Test
@@ -68,7 +69,7 @@ public class IndividualTest {
 
         Individual newIndividual = individual.mutate();
 
-        assertNotEquals(newIndividual.phenotypeForX(), individual.phenotypeForX(), 0.0);
+        assertNotEquals(newIndividual, individual);
     }
 
     @Test
@@ -83,5 +84,54 @@ public class IndividualTest {
 
         assertEquals(newIndividual.phenotypeForX(), individual.phenotypeForX(), 0.0);
         assertNotEquals(newIndividual.phenotypeForY(), individual.phenotypeForY(), 0.0);
+    }
+
+    @Test
+    public void cross_over_has_25_percent_chance_to_happen() throws Exception {
+        FakeRandom fakeRandom = new FakeRandom();
+        fakeRandom.valueForCall(0.20, 1);
+
+        Individual individual = new Individual(chromosome, fakeRandom);
+
+        Arrays.fill(chromosome, (byte) 1);
+
+        Individual other = new Individual(chromosome, fakeRandom);
+
+        List<Individual> offspring = individual.crossover(other);
+
+        assertTrue(offspring.isEmpty());
+    }
+
+    @Test
+    public void cross_over_generates_two_individuals() throws Exception {
+        FakeRandom fakeRandom = new FakeRandom();
+        fakeRandom.valueForCall(0.25, 1);
+        fakeRandom.valueForCall(0.9, 2);
+
+        Individual individual = new Individual(chromosome, fakeRandom);
+
+        Arrays.fill(chromosome, (byte) 1);
+
+        Individual other = new Individual(chromosome, fakeRandom);
+
+        List<Individual> offspring = individual.crossover(other);
+
+        Individual expectedChild = individualFromChromosome("111111111111111111110000");
+        Individual otherExpectedChild = individualFromChromosome("000000000000000000001111");
+
+        assertFalse(offspring.isEmpty());
+        assertEquals(expectedChild, offspring.get(0));
+        assertEquals(otherExpectedChild, offspring.get(1));
+    }
+
+
+    private Individual individualFromChromosome(String chromosome) {
+        byte[] actualChromosome = new byte[24];
+
+        for (int i = 0; i < chromosome.length(); i++) {
+            actualChromosome[i] = (byte) (chromosome.charAt(i) == '1' ? 1 : 0);
+        }
+
+        return Individual.fromChromosome(actualChromosome);
     }
 }
